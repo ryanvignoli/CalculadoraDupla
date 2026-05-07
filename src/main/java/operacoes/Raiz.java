71495305626dbc5584244931aa6128319eebc549
Ryan Nicolas <krameknicolas@gmail.com>
2026-05-07 17:21:10 -0300

    feat: implementa a funçao Raiz quadrada

---
+++ src/main/java/operacoes/Raiz.java
@@ -0,0 +1,11 @@
+package operacoes;
+
+import java.util.Scanner;
+
+public class Raiz {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(Math.sqrt(x));
+    }
+}

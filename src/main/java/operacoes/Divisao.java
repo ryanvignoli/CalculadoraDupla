f13181374bb5c53739910eabfe591f1a4637e502
Ryan Nicolas <krameknicolas@gmail.com>
2026-05-07 17:17:14 -0300

    feat: implementa a funçao Divisao

---
+++ src/main/java/operacoes/Divisao.java
@@ -0,0 +1,12 @@
+package operacoes;
+
+import java.util.Scanner;
+
+public class Divisao {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x / y);
+    }
+}
